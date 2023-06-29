eed3744b234e1bf8cfcad8bbed2d005fdc729ae6
hongkimsu <124145369+hongkimsu@users.noreply.github.com>
2023-06-29 17:37:54 +0900

    update metedata for jb (but still not work)

--- conf.py
+++ conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx_examples",
     "sphinxext.opengraph",
+    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,7 +46,16 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    "__pycache__",
+    "Thumbs.db",
+    ".DS_Store",
+    "*import_posts*",
+    "**/pandoc_ipynb/inputs/*",
+    ".nox/*",
+    "README.md",
+    ".vscode"
+]
 
 
 # -- Options for HTML output -------------------------------------------------
